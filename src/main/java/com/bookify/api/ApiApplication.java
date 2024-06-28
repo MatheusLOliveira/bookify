1a23c2917da8f57cc2b50926260bb458c45ff506
Math <matheuslimaoliveira@gmail.com>
2024-06-27 23:06:42 -0300

    fixing main class

--- src/main/java/com/bookify/api/ApiApplication.java
+++ src/main/java/com/bookify/api/ApiApplication.java
@@ -1,12 +1,30 @@
 package com.bookify.api;
 
+import com.bookify.api.controller.MenuController;
+import com.bookify.api.repository.AuthorRepository;
+import com.bookify.api.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication implements CommandLineRunner {
+
+	@Autowired
+	private BookRepository bookRepository;
+	@Autowired
+	private AuthorRepository authorRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		MenuController menuController = new MenuController(authorRepository, bookRepository);
+		menuController.showMenu();
 	}
 }
